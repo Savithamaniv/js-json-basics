aa608199f354b152dc61ce623fc0f73bf0d877d4
Savithamaniv <119982287+Savithamaniv@users.noreply.github.com>
2022-12-15 19:20:51 -0500

    JS file first commit

---
+++ jsfunctionbasics.js
@@ -0,0 +1,25 @@
+//Basic Jss variable declarions
+
+var name ='Savitha'
+name1  ='Mahe'
+var age= 10
+
+var email = 'savitest.com'
+var address ='11303 fiddlers creek, fishers,276589'
+console.log(name1)
+
+var num = 10+30
+ a = 50
+var x = a+10
+
+var foo = 'hello'+'welcome'
+var ishuman = true
+var z1=10
+var z2 = 5
+var z3 = z1-z2
+console.log(foo)
+console.log(10+40)
+console.log(a+45)
+var aa = 50
+var bb = 5
+console.log(aa*bb)
